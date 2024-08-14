7e8d697f009731f81345174b0b35df309ce8262f
AA0027 <whckdtjd456@naver.com>
2024-08-14 14:38:41 +0900

    컨트롤러경로api추가

--- src/main/java/org/example/com/controller/ChatRoomController.java
+++ src/main/java/org/example/com/controller/ChatRoomController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/channel")
+@RequestMapping("/api/channel")
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
