1069738c0b95c9c2bdc38bb2e74c0cab33de43c6
AA0027 <whckdtjd456@naver.com>
2024-08-15 16:27:04 +0900

    findroom 수정

--- src/main/java/org/example/com/domain/FindRoom.java
+++ src/main/java/org/example/com/domain/FindRoom.java
@@ -7,6 +7,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                columnNames={"chatRoom_id", "employee_id"}
+        )
+})
 public class FindRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
