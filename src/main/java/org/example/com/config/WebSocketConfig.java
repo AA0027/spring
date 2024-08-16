21eb86e6e5e184de5dc3fbc6315addf18f71f8bf
AA0027 <whckdtjd456@naver.com>
2024-08-16 22:00:14 +0900

    소켓연결경로수정

--- src/main/java/org/example/com/config/WebSocketConfig.java
+++ src/main/java/org/example/com/config/WebSocketConfig.java
@@ -33,7 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/channel")
+        registry.addEndpoint("/channel")
                 .setAllowedOrigins("*");
     }
 
