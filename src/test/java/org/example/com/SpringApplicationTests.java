e92db640d644dc464042193fda4569b0f6460d52
AA0027 <whckdtjd456@naver.com>
2024-08-19 09:43:29 +0900

    stomp로그 추가

--- src/test/java/org/example/com/SpringApplicationTests.java
+++ src/test/java/org/example/com/SpringApplicationTests.java
@@ -1,13 +1,28 @@
 package org.example.com;
 
+import org.example.com.domain.Employee;
+import org.example.com.repo.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringApplicationTests {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Test
     void contextLoads() {
+//        List<Employee> list = employeeRepository.findAll();
+//        list.forEach(e -> {
+//            e.setPassword(passwordEncoder.encode("1111"));
+//            employeeRepository.save(e);
+//        });
     }
 
 }
